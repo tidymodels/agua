a0656e7c9e3161e657eda434734c2fe5d95939c8
topepo <mxkuhn@gmail.com>
2022-05-17 17:55:10 -0400

    initial files

---
+++ R/h2o_train.R
@@ -0,0 +1,20 @@
+
+h2o_train <- function(x, y, model, ...) {
+  opts <- list(...)
+  x <- as.data.frame(x)
+  x_names <- names(x)
+  x$.outcome <- y
+  # x <- r_h2o(x)
+  mod_fun <- paste0("h2o.", model)
+  cl <-
+    rlang::call2(
+      mod_fun,
+      .ns = "h2o",
+      x = quote(x_names),
+      y = ".outcome",
+      training_frame = quote(x),
+      !!!opts
+    )
+  cl
+}
+
