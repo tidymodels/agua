9ecc85e983e16eff2f3280120ff4584f52f94720
Max Kuhn <mxkuhn@gmail.com>
2022-05-18 10:58:24 -0400

    more infrastructure functions

---
+++ R/agua-package.R
@@ -0,0 +1,16 @@
+#' tidymodels integration with h2o
+#'
+#' agua has tools that allows users to fit and tune models using the h2o
+#' platform. Most functions in this package are not user facing; agua enables
+#' new parsnip model engines and sets up additional infrastructure for tune.
+#'
+#' The package uses code initially written by Steven Pawley in his h2oparsnip
+#' package. Addition work was done by Qiushi Yan as an RStudio summer intern.
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom parsnip set_model_arg
+#' @importFrom tune tune
+## usethis namespace: end
+NULL
--- R/misc.R
+++ R/misc.R
@@ -2,15 +2,56 @@
 # ------------------------------------------------------------------------------
 # Data conversions
 
+#' Data conversion tools
+#' @param x A R data frame (for `r_h2o`) or an H2OFrame (`h2o_r`).
+#' @return A tibble or H2OFrame.
+#' @examples
+#'
+#' # start with h2o::h2o.init()
+#' if (h2o_running()) {
+#'   cars2 <- r_h2o(mtcars)
+#'   cars2
+#'
+#'   cars0 <- h2o_r(cars2)
+#'   cars0
+#'
+#' }
+#' @export
 r_h2o <- function(x) {
   h2o::as.h2o(x)
   # TODO find a way to do this quietly withr::with_options and purrr::quietly
 }
 
+#' @export
+#' @rdname r_h2o
 h2o_r <- function(x) {
-  as.data.frame(x)
-  # TODO to tibble
+  x <- as.data.frame(x)
+  tibble::as_tibble(x)
 }
+
+# ------------------------------------------------------------------------------
+
+#' Check if h2o cluster is initialized
+#'
+#' @param verbose Print out the message if no cluster is available.
+#' @return A logical.
+#' @examples
+#' h2o_running()
+#' h2o_running(verbose = TRUE)
+#' @export
+h2o_running <- function(verbose = FALSE) {
+  res <- try(h2o::h2o.clusterIsUp(), silent = TRUE)
+  if (inherits(res, "try-error")) {
+    if (verbose) {
+      msg <- as.character(res)
+      rlang::inform(msg)
+    }
+    res <- FALSE
+  }
+  res
+}
+
+
 
 # ------------------------------------------------------------------------------
 
