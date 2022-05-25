5453bba338a8651bb400848b3f6cf01727a6f2e1
qiushi <qiushi.yann@gmail.com>
2022-05-25 10:55:09 -0500

    rename grid to h2o names

--- R/utils.R
+++ R/utils.R
@@ -31,3 +31,21 @@ is_h2o <- function(workflow, ...) {
   model_spec <- hardhat::extract_spec_parsnip(object)
   identical(model_spec$engine, "h2o")
 }
+
+rename_grid_h2o <- function(grid, workflow) {
+  model_spec <- hardhat::extract_spec_parsnip(workflow)
+  # For translate from given names/ids in grid to parsnip names:
+  params <- model_spec %>% extract_parameter_set_dials()
+  params <- tibble::as_tibble(params)
+  pset <- params$id
+  names(pset) <- params$name
+  grid_parsnip <- dplyr::rename(grid, !!!pset)
+
+  # Go from parsnip names to h2o names
+  arg_key <- get_from_env(paste0(class(model_spec)[1], "_args")) %>%
+    dplyr::filter(engine == "h2o")
+  # rename again
+  pset <- arg_key$parsnip %>% purrr::set_names(arg_key$original)
+  grid_h2o <- dplyr::rename(grid_parsnip, !!!pset)
+  grid_h2o
+}
