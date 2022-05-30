52a6511285284cd14bc2351fb778779b2ee7d7d5
qiushi <qiushi.yann@gmail.com>
2022-05-30 09:49:56 -0500

    fix devtools::check issues

--- R/agua-package.R
+++ R/agua-package.R
@@ -19,6 +19,7 @@ NULL
 
 utils::globalVariables(
   c(
-    ".iter_config", ".iter_preprocessor", "data", "engine", "out_notes", "role"
+    ".iter_config", ".iter_preprocessor", ".iter_model", "data", "engine",
+    "out_notes", "role", "parsnip", ".msg_model", "original"
   )
 )
